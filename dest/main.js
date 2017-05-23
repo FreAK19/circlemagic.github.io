41f12ef7d8d09a9c228f5d58b956c5a3491e4392
FreAK19 <andrii.kiri4encko@yandex.ru>
2017-05-23 10:52:27 +0300

    add preventDefault()

--- dest/main.js
+++ dest/main.js
@@ -2,10 +2,9 @@
  * Created by Andrew on 22.05.2017.
  */
 
-$(document).ready(function(){
-    $(".btn").on("click", function () {
-        let id  = $(this).attr('href'),
+    $(".btn").on("click", function (event) {
+		event.preventDefault();
+        var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top - 60}, 1500);
     });
-});
