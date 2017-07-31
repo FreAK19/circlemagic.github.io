a0a4470dc886098385aa4bc3121c709d83bc1023
FreAK19 <andrii.kiri4encko@yandex.ru>
2017-07-31 15:15:02 +0300

    remove old files

--- dest/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Andrew on 22.05.2017.
- */
-
-    $(".btn").on("click", function (event) {
-		let top = window.innerHeight;
-		console.log(top);
-        $('body,html').animate({scrollTop: top + 20}, 1500);
-    });
