2c7da1c8c25a1bebc5f59e9359fab76e5674eb4c
FreAK19 <andrii.kiri4encko@yandex.ru>
2017-07-31 15:15:17 +0300

    #gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,42 +10,45 @@ const jsmin = require('gulp-jsmin');
 const csso = require('gulp-csso');
 
 
-let image_prop = {
-    width : 1600,
-    height : 700,
-    crop : true,
-    upscale : false
+const image_prop = {
+  width: 1600,
+  height: 700,
+  crop: true,
+  upscale: false
 };
 
-gulp.task('image', () => {
-    gulp.src(['src/img/*.png', 'src/img/*.jpg'])
-        .pipe(imagemin({
-            optimizationLevel: 10,
-        }))
-        .pipe(imageResize(image_prop))
-        .pipe(gulp.dest('dest/img'));
-});
+gulp.task('image', function () {
+  gulp.src(['src/img/*.png', 'src/img/*.jpg'])
+  .pipe(imagemin({
+    optimizationLevel: 10,
+  }))
+  .pipe(imageResize(image_prop))
+  .pipe(gulp.dest('dist/img'));
+})
+;
 
 
-gulp.task('css', () => {
-    gulp.src('src/css/*.css')
-        .pipe(autoprefixer({
-            browsers: ['last 3 versions'],
-            cascade: false
-        }))
-        .pipe(csso())
-        .pipe(concatCSS('dest/css/style.css'))
-        .pipe(gulp.dest(''));
-});
+gulp.task('css', function () {
+  gulp.src('src/css/*.css')
+  .pipe(autoprefixer({
+    browsers: ['last 3 versions'],
+    cascade: false
+  }))
+  .pipe(csso())
+  .pipe(concatCSS('dist/css/style.css'))
+  .pipe(gulp.dest(''));
+})
+;
 
-gulp.task('js', ()=> {
-    gulp.src('src/circleMagic.js')
-        .pipe(jsmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dest'));
-});
+gulp.task('js', function () {
+  gulp.src('src/circleMagic.js')
+  .pipe(jsmin())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('dist'));
+})
+;
 
-gulp.task('watch', () => {
-    gulp.watch('src/css/*.css', ['css']);
-    gulp.watch('src/circleMagic.js', ['js']);
+gulp.task('watch', function () {
+  gulp.watch('src/css/*.css', ['css']);
+gulp.watch('src/circleMagic.js', ['js']);
 });
